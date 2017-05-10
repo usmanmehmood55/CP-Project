b9bd8ec69c1f1adfc85bc38158ed15994afc66c4
Usman Mehmood <160979@students.au.edu.pk>
2017-05-10 09:49:28 +0500

    Add files via upload

--- CPProject.cpp
+++ CPProject.cpp
@@ -1,13 +1,13 @@
 /*
- * CPProject.cpp
+ * CP-Project.cpp
  *
- *  Created on: Apr 9, 2017
- *      Author: Usman
+ *  Created on: May 10, 2017
+ *      Author: asim
  */
-
 #include <iostream>
 #include <fstream>
 #include <sstream> //Only used for converting int to string.
+#include <cstdio> //For removing files.
 using namespace std;
 void add(int rollnum)
 {
@@ -99,11 +99,13 @@ void display()
 	string address[4];
 	string line;
 
-	cout<<"Enter roll number."<<endl;
-	string rollnum; cin>>rollnum;
 
 	//Reading from the database file.
-	ifstream database ("<<rollnum<<.txt");
+	cout<<"Enter roll number."<<endl;
+	string rollnum; cin>>rollnum;
+    rollnum += ".txt";
+
+	ifstream database (rollnum.c_str(), ios::app);
 	if (database.is_open())
 	{ while( database>>name[0]>>name[1]>>dob[0]>>dob[1]>>dob[2]>>age>>address[0]>>address[1]>>address[2]>>address[3] )
 		database.close();
@@ -161,6 +163,105 @@ void roll()
 
 
 
+void edit()
+{
+	cout<<"Please enter the roll number of student: ";
+	int rollnum; cin>>rollnum; cout<<endl;
+	cout<<"Please enter new data about the student: "<<endl;
+
+	//Name
+	string name[2];
+	cout<<"First name:\t"<<endl;	cin>>name[0];
+	cout<<"Last name:\t"<<endl;		cin>>name[1];
+	cout<<endl;
+
+	//Date of birth.
+	int dob[3];
+	cout<<"Date of birth:-"<<endl;
+	cout<<"Date:\t"<<endl;		cin>>dob[0];
+	cout<<"Month:\t"<<endl;		cin>>dob[1];
+	cout<<"Year:\t"<<endl;		cin>>dob[2];
+	cout<<endl;
+
+	//Age.
+	int age;
+	age=(2017*12-((dob[2]*12)+dob[1]))/12; //Only accurate for months.
+
+	//Address.
+	string address[4];
+	cout<<"Address:-"<<endl;
+	cout<<"City:\t"<<endl;		cin>>address[0];
+	cout<<"Colony:\t"<<endl;		cin>>address[1];
+	cout<<"Street:\t"<<endl;		cin>>address[2];
+	cout<<"House:\t"<<endl;		cin>>address[3];
+	cout<<endl;
+
+	//Courses.
+	cout<<"Enter number of courses: "; int coursenum; cin>>coursenum;
+	string course[coursenum], grade[coursenum]; int marks[coursenum];
+	for (int temp1;temp1<coursenum;temp1++)
+	{
+		cout<<"Enter name of course "<<temp1+1<<":\t"<<endl;	cin>>course[temp1];
+		cout<<"Enter marks in that course.\t"<<endl;			cin>>marks[temp1];
+		if (marks[temp1] <= 100 && marks[temp1] >= 80)
+		{
+			grade [temp1] = 'A';
+		} else if  (marks[temp1] <= 79 && marks[temp1] >= 60)
+		{
+			grade [temp1] = 'B';
+		} else if (marks[temp1] <= 59 && marks[temp1] >= 50)
+		{
+			grade [temp1] = 'C';
+		} else if (marks[temp1] <= 49 && marks[temp1] >= 40)
+		{
+			grade [temp1] = 'D';
+		} else if (marks[temp1] <= 39)
+		{
+			grade [temp1] = 'F';
+		}
+	}
+
+	cout<<"Editing the file of roll number "<<rollnum<<endl;
+
+
+
+	//Making a file and writing to it.
+	int number=rollnum;
+	string rolli;
+	stringstream convert;
+	convert << number;
+	rolli = convert.str();
+
+    // remove( rolli, ios::in ); Insert remove function
+
+
+    ofstream database;
+    database.open(rolli.c_str(), ios::app);
+
+	int temp2;
+	database <<name[0]<<endl<<name[1]<<endl<<dob[0]<<endl<<dob[1];
+	database <<endl<<dob[2]<<endl<<age<<endl<<address[0]<<endl<<address[1];
+	database <<endl<<address[2]<<endl<<address[3]<<endl;
+	for (temp2=0; temp2<coursenum; temp2++)
+	{
+		database <<course[temp2]<<endl<<grade[temp2]<<endl;
+	}
+	database.close();
+}
+
+
+
+void removeit()
+{
+	cout<<"Enter roll number: ";
+	string rollnum; cin>>rollnum;
+    rollnum += ".txt";
+
+	// int remove ( rollnum.c_str(), ios::app ); Insert remove function.
+}
+
+
+
 void menu()
 {
 	//Main menu.
@@ -176,6 +277,12 @@ void menu()
 		break;
 	case 2:
 		display();
+		break;
+	case 3:
+		edit();
+		break;
+	case 4:
+		removeit();
 		break;
 	}
 }
@@ -200,3 +307,6 @@ int main()
 	cout<<"Exiting";
 	return 0;
 }
+
+
+
